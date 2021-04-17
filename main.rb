720c3776279369a994f2087004120a2086c2375d
knmrftw <2669997@gmail.com>
2021-04-17 18:01:10 +0300

    comment translated

--- main.rb
+++ main.rb
@@ -10,23 +10,23 @@ end
 require_relative 'lib/console_interface'
 require_relative 'lib/game'
 
-# 1. Поздороваться
+# 1. Greeting
 puts 'Hello everyone!'
 
-# 2. Загрузить случайное слово из файла
+# 2. Downloading the random word to play
 word = File.readlines("#{__dir__}/data/words.txt", encoding: 'UTF-8', chomp: true).sample
 game = Game.new(word)
 console_interface = ConsoleInterface.new(game)
 
-# 3. Пока не закончилась игра
+# 3. Until game is over
 until game.over?
-  #   3.1. Вывести очередное состояние игры
+  #   3.1. Show the current condition of the game
   console_interface.print_out
-  #   3.2. Спросить очередную букву
+  #   3.2. Ask next letter to play
   letter = console_interface.get_input
-  #   3.3. Обновить состояние игры
+  #   3.3. Update current condition of the game
   game.play!(letter)
 end
 
-# 4. Вывести финальное состояние игры
+# 4. Final condition of the game
 console_interface.print_out
